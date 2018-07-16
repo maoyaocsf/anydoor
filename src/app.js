8b054d7a9432a38d753560ed35a031bdbbb449b9
maoyaocsf <1547711850@qq.com>
2018-07-16 19:58:42 +0800

    'two'

--- src/app.js
+++ src/app.js
@@ -4,40 +4,52 @@ const path = require('path');
 
 const conf = require('./config/defaultConfig');
 const route = require('./helper/route');
-const server = http.createServer((req, res)=>{
+
+class Server {
+    constructor(config){
+        this.conf = Object.assign({},conf,config);
+    }
+    start(){
+
+        const server = http.createServer((req, res)=>{
     
-    const filePath = path.join(conf.root,req.url);
+            const filePath = path.join(this.conf.root,req.url);
+        
+            route(req,res,filePath,this.conf);
+            // fs.stat(filePath,(err,stats)=>{
+            //     if(err){
+            //         res.statusCode = 404;
+            //         res.setHeader('Content-Type','text/plain');
+            //         res.end(`${filePath} is not a directory or file`);
+            //         return;
+            //     }
+            //     if(stats.isFile()){
+            //         res.statusCode=200;
+            //         res.setHeader('Content-Type','text/plain');
+            //         // fs.readFile(filePath,(err,data)=>{
+            //         //     if(err) throw err;
+            //         //     res.end(data);
+            //         // });
+            //         fs.createReadStream(filePath).pipe(res);
+            //     }else if(stats.isDirectory()){
+            //         fs.readdir(filePath,(err,files)=>{
+            //             res.statusCode=200;
+            //             res.setHeader('Content-Type','text/plain');
+            //             res.end(files.join(','));
+            //         });
+            //     }
+            //})
+            // res.statusCode = 200;
+            // res.setHeader('Content-Type','text/plain');
+            // res.end(filePath);
+        });
+        
+        server.listen(this.conf.port,this.conf.hostname,()=>{
+            const addr = `http://${this.conf.hostname}:${this.conf.port}`;
+            console.log(`Server started at ${chalk.green(addr)}`);
+        })
+        
+    }
+}
 
-    route(req,res,filePath);
-    // fs.stat(filePath,(err,stats)=>{
-    //     if(err){
-    //         res.statusCode = 404;
-    //         res.setHeader('Content-Type','text/plain');
-    //         res.end(`${filePath} is not a directory or file`);
-    //         return;
-    //     }
-    //     if(stats.isFile()){
-    //         res.statusCode=200;
-    //         res.setHeader('Content-Type','text/plain');
-    //         // fs.readFile(filePath,(err,data)=>{
-    //         //     if(err) throw err;
-    //         //     res.end(data);
-    //         // });
-    //         fs.createReadStream(filePath).pipe(res);
-    //     }else if(stats.isDirectory()){
-    //         fs.readdir(filePath,(err,files)=>{
-    //             res.statusCode=200;
-    //             res.setHeader('Content-Type','text/plain');
-    //             res.end(files.join(','));
-    //         });
-    //     }
-    //})
-    // res.statusCode = 200;
-    // res.setHeader('Content-Type','text/plain');
-    // res.end(filePath);
-});
-
-server.listen(conf.port,conf.hostname,()=>{
-    const addr = `http://${conf.hostname}:${conf.port}`;
-    console.log(`Server started at ${chalk.green(addr)}`);
-})
+module.exports = Server;
--- src/helper/route.js
+++ src/helper/route.js
@@ -4,19 +4,44 @@ const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
-const conf = require('../config/defaultConfig');
+// const conf = require('../config/defaultConfig');
+const mime = require('./mine');
+const compress = require('./compress');
+const range = require('./range');
+const isFresh = require('./cache');
 
 const tplPath = path.join(__dirname,'../template/dir.html');
 const source = fs.readFileSync(tplPath);
 const template = Handlebars.compile(source.toString());
 
-module.exports = async function (req,res,filePath) {
+module.exports = async function (req,res,filePath,conf) {
     try {
         const stats =await stat(filePath);
         if(stats.isFile()){
-            res.statusCode=200;
-            res.setHeader('Content-Type','text/plain');
-            fs.createReadStream(filePath).pipe(res);
+            const contentType = mime(filePath);
+            
+            res.setHeader('Content-Type',contentType);
+            
+            if(isFresh(stats,req,res)){
+                res.statusCode = 304;
+                res.end();
+                return;
+            }
+
+            let rs;
+            const {code,start,end} = range(stats.size,req,res);
+            if(code ===200){
+                res.statusCode=200;
+                rs = fs.createReadStream(filePath);
+            }else{
+                res.statusCode=206;
+                rs = fs.createReadStream(filePath,{start,end});
+            }
+
+            if(filePath.match(conf.compress)){
+                rs = compress(rs,req,res);
+            }
+            rs.pipe(res);
         }else if(stats.isDirectory()){
 
             const files =await readdir(filePath);
@@ -26,7 +51,12 @@ module.exports = async function (req,res,filePath) {
             const data = {
                 title:path.basename(filePath),
                 dir:dir?`/${dir}`:'',
-                files
+                files:files.map(file=>{
+                    return {
+                        file,
+                        icon:mime(file)
+                    }
+                })
             };
             res.end(template(data));
 
